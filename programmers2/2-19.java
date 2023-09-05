6adfd5dca26b6f048224137a7024868f8b5c0ca5
Nara Park <121935031+nara316@users.noreply.github.com>
2023-09-05 22:31:58 +0900

    의상

---
+++ programmers2/2-19.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        for (Map.Entry<String,Integer> entry : map.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }    
+        
+        return answer - 1;
+    }
+}
