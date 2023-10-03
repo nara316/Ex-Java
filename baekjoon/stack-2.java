b6d7ecb28055b47d23c8cb0334d74e88461615ca
Nara Park <121935031+nara316@users.noreply.github.com>
2023-10-03 23:27:53 +0900

    스택

--- baekjoon/stack-2.java
+++ baekjoon/stack-2.java
@@ -13,7 +13,7 @@ public class Main {
 
         int count = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<count; i++){
+        for (int i=0; i<count; i++) {
 
             Stack<Character> stack = new Stack<>();
 
@@ -22,19 +22,19 @@ public class Main {
 
             int inputLength = input.length();
 
-            for(int j=0; j<input.length(); j++){
+            for (int j=0; j<input.length(); j++) {
                 char[] inputArray = input.toCharArray();
-                if(inputArray[j] == '('){
+                if (inputArray[j] == '(') {
                     stack.push('(');
                 }
-                else{
+                else {
                     if(stack.empty()) break;
                     else stack.pop();
                 }
                 inputLength--;
             }
 
-            if(stack.empty() && (inputLength == 0)){
+            if (stack.empty() && (inputLength == 0)) {
                 System.out.println("YES");
             }
             else System.out.println("NO");
