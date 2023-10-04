bf9e2e6c6b043ad6184f29b7932e2b20968cca5d
Nara Park <121935031+nara316@users.noreply.github.com>
2023-10-04 23:14:03 +0900

    문자의 합

--- baekjoon/11720.java
+++ baekjoon/11720.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
- 
+
 public class Main {
-    public static void main(String [] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        String y = sc.next();
-        int result = 0;
-        
-        sc.close();
-        
-        for (int i=0; i<x; i++) {
-            result += Integer.parseInt(y.substring(i, i+1));
-        }
-        
-        System.out.println(result);
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String inputs = sc.next();
+		sc.close();
+		
+		int result = 0;
+		for (int i = 0; i < n; ++i) {
+			result += inputs.charAt(i) - '0';
+		}
+		System.out.println(result);
+	}
 }
