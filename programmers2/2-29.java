26886f202daef1f288baaecc1025e801f6b21e33
Nara Park <skfk6352@naver.com>
2023-10-06 00:08:21 +0900

    잔디심기 왜 안되는거지

--- programmers2/2-29.java
+++ programmers2/2-29.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int test(int[] scoville, int K) {
+    public int answer(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int answer = 0;
         
